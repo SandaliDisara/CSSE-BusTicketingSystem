01e0aa019da8a4fb20c30ae663112b6050faa4e8
Sandali Disara <sandalidissanayake123@gmail.com>
2023-10-21 20:08:08 +0530

    modified the test case files

--- components/TestCases/BusInfo.test.js
+++ components/TestCases/BusInfo.test.js
@@ -1,48 +1,63 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import BusInfo from '../BusInfo';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import BusInfo from "../BusInfo";
 
-describe('BusInfo', () => {
-  it('renders the BusInfo component correctly', () => {
-    const { getByText, getByPlaceholderText, getByTestId } = render(<BusInfo />);
-    
-    // Check that the component renders the expected text and elements
-    expect(getByText('Bus Number:')).toBeTruthy();
-    expect(getByText('From:')).toBeTruthy();
-    expect(getByText('To:')).toBeTruthy();
-    expect(getByText('Price:')).toBeTruthy();
-    expect(getByText('Book Journey')).toBeTruthy();
-    expect(getByText('Stopping Stations -')).toBeTruthy();
-    
-    // Check that the "Download QR Code" button is initially hidden
-    const downloadButton = getByTestId('download-button');
+describe("BusInfo", () => {
+  it("renders the BusInfo component correctly", () => {
+    const { getByText, getByTestId } = render(
+      <BusInfo
+        route={{
+          params: {
+            busNo: "BU1839",
+            from: "kalutara",
+            to: "colombo",
+            price: "Rs. 550",
+            stations: "T1, T2, B4, B5",
+          },
+        }}
+      />
+    );
+
+    expect(getByText("Bus Number:")).toBeTruthy();
+    expect(getByText("From:")).toBeTruthy();
+    expect(getByText("To:")).toBeTruthy();
+    expect(getByText("Price:")).toBeTruthy();
+    expect(getByText("Book Journey")).toBeTruthy();
+    expect(getByText("Stopping Stations -")).toBeTruthy();
+
+    const downloadButton = getByTestId("download-button");
     expect(downloadButton).toBeTruthy();
     expect(downloadButton).not.toBeVisible();
-    
-    // Check that the QR code text is initially displayed
-    expect(getByText('Book Journey to \ngenerate QR code')).toBeTruthy();
+
+    expect(getByText("Book Journey to \ngenerate QR code")).toBeTruthy();
   });
 
   it('toggles between "Book Journey" and "Download QR Code" on button click', () => {
-    const { getByText, getByTestId } = render(<BusInfo />);
-    
-    // Click the "Book Journey" button
-    fireEvent.press(getByText('Book Journey'));
-    
-    // Check that the "Download QR Code" button is now visible
-    const downloadButton = getByTestId('download-button');
+    const { getByText, getByTestId } = render(
+      <BusInfo
+        route={{
+          params: {
+            busNo: "BU1839",
+            from: "kalutara",
+            to: "colombo",
+            price: "Rs. 550",
+            stations: "T1, T2, B4, B5",
+          },
+        }}
+      />
+    );
+
+    fireEvent.press(getByText("Book Journey"));
+
+    const downloadButton = getByTestId("download-button");
     expect(downloadButton).toBeVisible();
-    
-    // Check that the QR code text is no longer displayed
-    expect(getByText('Book Journey to \ngenerate QR code')).not.toBeVisible();
-    
-    // Click the "Download QR Code" button
+
+    expect(getByText("Book Journey to \ngenerate QR code")).not.toBeVisible();
+
     fireEvent.press(downloadButton);
-    
-    // Check that the "Download QR Code" button is no longer visible
+
     expect(downloadButton).not.toBeVisible();
-    
-    // Check that the QR code text is displayed again
-    expect(getByText('Book Journey to \ngenerate QR code')).toBeVisible();
+
+    expect(getByText("Book Journey to \ngenerate QR code")).toBeVisible();
   });
 });
--- components/TestCases/BusList.test.js
+++ components/TestCases/BusList.test.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import BusList from '../BusList';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import BusList from "../BusList";
 
-describe('BusList', () => {
-  it('renders the BusList component correctly', () => {
-    const { getByText, getByPlaceholderText, getAllByTestId } = render(<BusList />);
-    
+describe("BusList", () => {
+  it("renders the BusList component correctly", () => {
+    const { getByText, getByPlaceholderText, getAllByTestId } = render(
+      <BusList />
+    );
+
     // Check that the component renders the expected text and input elements
-    expect(getByText('Enter Your Journey')).toBeTruthy();
-    expect(getByText('Choose Transport')).toBeTruthy();
-    expect(getByPlaceholderText('From')).toBeTruthy();
-    expect(getByPlaceholderText('To')).toBeTruthy();
-    
+    expect(getByText("Enter Your Journey")).toBeTruthy();
+    expect(getByText("Choose Transport")).toBeTruthy();
+    expect(getByPlaceholderText("From")).toBeTruthy();
+    expect(getByPlaceholderText("To")).toBeTruthy();
+
     // Check that the "Search Journey" button is rendered
-    expect(getByText('Search Journey')).toBeTruthy();
-    
+    expect(getByText("Search Journey")).toBeTruthy();
+
     // Check that at least one bus item is rendered
-    const busItems = getAllByTestId('bus-item');
+    const busItems = getAllByTestId("bus-item");
     expect(busItems.length).toBeGreaterThan(0);
   });
 
   it('navigates to the Bus Information screen on "Book" button click', () => {
     const { getByText } = render(<BusList />);
-    
+
     // Mock the useNavigation hook
     const navigateMock = jest.fn();
-    jest.mock('@react-navigation/native', () => ({
+    jest.doMock("@react-navigation/native", () => ({
       useNavigation: () => ({ navigate: navigateMock }),
-      useRoute: () => ({ params: { userName: 'Test User' } }),
+      useRoute: () => ({ params: { userName: "Test User" } }),
     }));
 
     // Click the "Book" button on the first bus item
-    fireEvent.press(getByText('Book'));
+    fireEvent.press(getByText("Book"));
 
     // Check that the navigate function was called with the correct screen name and route params
-    expect(navigateMock).toHaveBeenCalledWith('Bus Information', {
-      busNo: 'Bus Number', // Replace with actual bus number
-      from: 'From', // Replace with actual from location
-      to: 'To', // Replace with actual to location
-      price: 0, // Replace with actual price
-      stations: 'Stations', // Replace with actual stations
+    expect(navigateMock).toHaveBeenCalledWith("Bus Information", {
+      busNo: "BU1839",
+      from: "kalutara",
+      to: "colombo",
+      price: "Rs. 550",
+      stations: "T1, T2, B4, B5",
     });
   });
 });
